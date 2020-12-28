495a0ea3cbee8a5f3c024f51fc954d55a6ac2fef
deepaksnegi <46718177+deepaksnegi@users.noreply.github.com>
2020-12-28 23:34:07 +0530

    added basic project structure (#1)
    * "added basic structure"

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Layout from "./component/layout/Layout";
+import BurgerBuilder from "./containers/burgerBuilder/BurgerBuilder";
+
+function App() {
+  return (
+    <Layout>
+      <BurgerBuilder />
+    </Layout>
+  );
+}
+
+export default App;
